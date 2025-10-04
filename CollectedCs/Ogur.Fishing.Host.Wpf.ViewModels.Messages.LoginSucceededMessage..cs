5e0ca3388c4667b22ba3072490ef64a1fd9f2eb3
ishkabar <dom.karczewski@gmail.com>
2025-10-04 21:10:46 +0200

    chore(git): remove CollectedCs temp backup directory

--- CollectedCs/Ogur.Fishing.Host.Wpf.ViewModels.Messages.LoginSucceededMessage..cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Ogur.Fishing.Host.Wpf.ViewModels.Messages;
-
-
-/// <summary>
-/// Message indicating that a user successfully signed in.
-/// </summary>
-public sealed record LoginSucceededMessage(string Username);
